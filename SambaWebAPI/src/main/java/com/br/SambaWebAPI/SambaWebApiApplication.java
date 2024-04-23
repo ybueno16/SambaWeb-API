698a0971247d060886d332675cd0948267a486e0
ybueno16 <yuribueno27@gmail.com>
2024-04-22 22:30:33 -0300

    Migraçao API para Spring Boot

---
+++ SambaWebAPI/src/main/java/com/br/SambaWebAPI/SambaWebApiApplication.java
@@ -0,0 +1,13 @@
+package com.br.SambaWebAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SambaWebApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SambaWebApiApplication.class, args);
+	}
+
+}
