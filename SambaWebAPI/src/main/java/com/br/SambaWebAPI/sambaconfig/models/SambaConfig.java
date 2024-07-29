da2438b2335b86a6c9d6566064e4aae3d859a3fb
ybueno16 <yuribueno27@gmail.com>
2024-07-29 17:32:47 -0300

    feat: adicionar model sambaconfig

---
+++ SambaWebAPI/src/main/java/com/br/SambaWebAPI/sambaconfig/models/SambaConfig.java
@@ -0,0 +1,24 @@
+package com.br.SambaWebAPI.sambaconfig.models;
+
+import java.util.List;
+
+public class SambaConfig {
+    private String section;
+    private List<String> sectionParams;
+
+    public String getSection() {
+        return section;
+    }
+
+    public void setSection(String section) {
+        this.section = section;
+    }
+
+    public List<String> getSectionParams() {
+        return sectionParams;
+    }
+
+    public void setSectionParams(List<String> sectionParams) {
+        this.sectionParams = sectionParams;
+    }
+}
