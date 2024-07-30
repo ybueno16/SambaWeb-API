a375a16ed4fd1ae3c6dad2944677bf9c5800849d
vr-yuribueno <yuri.bueno@vrsoft.com.br>
2024-07-30 10:11:49 -0300

    feat: Criar controller para realizar configuração no arquivo smb.conf

---
+++ SambaWebAPI/src/main/java/com/br/SambaWebAPI/sambaconfig/controller/SambaConfigController.java
@@ -0,0 +1,57 @@
+package com.br.SambaWebAPI.sambaconfig.controller;
+
+import com.br.SambaWebAPI.config.Global;
+import com.br.SambaWebAPI.config.ResponseEntity.DefaultResponseEntityFactory;
+import com.br.SambaWebAPI.password.models.SudoAuthentication;
+import com.br.SambaWebAPI.sambaconfig.models.SambaConfig;
+import com.br.SambaWebAPI.sambaconfig.services.SambaConfigService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+import java.util.Map;
+
+@RestController
+@RequestMapping(Global.API_URL_SAMBA +"/samba-config")
+public class SambaConfigController {
+    final private ObjectMapper objectMapper;
+    final private SambaConfigService sambaConfigService;
+
+    @Autowired
+    public SambaConfigController(ObjectMapper objectMapper, SambaConfigService sambaConfigService){
+        this.objectMapper = objectMapper;
+        this.sambaConfigService = sambaConfigService;
+
+
+    }
+
+    @PostMapping(path = "/configureSambaFile")
+    public ResponseEntity<?> configureSambaFile(@RequestBody Map<String,Object> json){
+        SambaConfig sambaConfig = objectMapper.convertValue(json.get("section"), SambaConfig.class);
+        SudoAuthentication sudoAuthentication = objectMapper.convertValue(json.get("sudoAuthentication"), SudoAuthentication.class);
+
+        try{
+            sambaConfigService.sambaConfigWriteNewConfig(sambaConfig,sudoAuthentication);
+            return DefaultResponseEntityFactory.create("Configuração salva com sucesso!", sambaConfig, HttpStatus.OK);
+
+
+        } catch (IOException e) {
+            return DefaultResponseEntityFactory.create(
+                    "Ocorreu um erro na escrita do arquivo. " + e,
+                    null,
+                    HttpStatus.INTERNAL_SERVER_ERROR);
+        } catch (Exception e){
+            return DefaultResponseEntityFactory.create(
+                    "Erro genérico. Ocorreu um erro desconhecido durante a criação da pasta.",
+                    null,
+                    HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+    }
+}
--- SambaWebAPI/src/main/java/com/br/SambaWebAPI/sambaconfig/services/SambaConfigService.java
+++ SambaWebAPI/src/main/java/com/br/SambaWebAPI/sambaconfig/services/SambaConfigService.java
@@ -2,6 +2,8 @@ package com.br.SambaWebAPI.sambaconfig.services;
 
 
 import com.br.SambaWebAPI.config.Global;
+import com.br.SambaWebAPI.password.models.SudoAuthentication;
+import com.br.SambaWebAPI.sambaconfig.models.SambaConfig;
 import org.springframework.stereotype.Service;
 
 import java.io.*;
@@ -9,24 +11,24 @@ import java.util.Arrays;
 
 @Service
 public class SambaConfigService {
-    public void sambaConfigWriteNewConfig(String section,String... sectionParams) throws IOException {
+    public void sambaConfigWriteNewConfig(SambaConfig sambaConfig, SudoAuthentication sudoAuthentication) throws IOException {
         BufferedWriter writer = new BufferedWriter(new FileWriter(Global.SMB_CONF_PATH,true));
         BufferedReader reader = new BufferedReader(new FileReader(Global.SMB_CONF_PATH));
         try {
             String line;
 
             while ((line = reader.readLine()) != null) {
-                if(line.contains("[" + section + "]")){
+                if(line.contains("[" + sambaConfig.getSection() + "]")){
                     System.out.println(line);
-                    for (String sectionParam : sectionParams) {
+                    for (String sectionParam : sambaConfig.getSectionParams()) {
                         writer.newLine();
                         writer.write(Arrays.toString((sectionParam + "\n").toCharArray()));
                     }
                 } else {
                     System.out.println(line);
                     writer.newLine();
-                    writer.write("[" + section + "]" + "\n");
-                    for (String sectionParam : sectionParams) {
+                    writer.write("[" + sambaConfig.getSection() + "]" + "\n");
+                    for (String sectionParam : sambaConfig.getSectionParams()) {
                         writer.write(Arrays.toString((sectionParam + "\n").toCharArray()));
                     }
                 }
