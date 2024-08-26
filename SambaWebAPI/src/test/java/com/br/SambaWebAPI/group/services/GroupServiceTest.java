78bf29025e94362b2068786943994df92447d981
vr-yuribueno <yuri.bueno@vrsoft.com.br>
2024-08-26 18:23:58 -0300

    refactor: arrumar indentação

--- SambaWebAPI/src/test/java/com/br/SambaWebAPI/group/services/GroupServiceTest.java
+++ SambaWebAPI/src/test/java/com/br/SambaWebAPI/group/services/GroupServiceTest.java
@@ -15,7 +15,6 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
