de48e80baaf63790d82ad4cc92e5fab088a19d23
vr-yuribueno <yuri.bueno@vrsoft.com.br>
2024-08-31 16:45:03 -0300

    feat: adicionar teste de integração

--- SambaWebAPI/src/main/java/com/br/SambaWebAPI/config/DataSourceConfig.java
+++ SambaWebAPI/src/main/java/com/br/SambaWebAPI/config/DataSourceConfig.java
@@ -1,19 +1,87 @@
 package com.br.SambaWebAPI.config;
 
 import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 @Configuration
 public class DataSourceConfig {
+  @Value("${postgres.driver}")
+  private String driver;
+
+  @Value("${postgres.ip}")
+  private String ip;
+
+  @Value("${postgres.port}")
+  private String port;
+
+  @Value("${postgres.database}")
+  private String database;
+
+  @Value("${postgres.username}")
+  private String username;
+
+  @Value("${postgres.password}")
+  private String password;
+
   @Bean
   public DataSource dataSource() {
     DriverManagerDataSource dataSource = new DriverManagerDataSource();
     dataSource.setDriverClassName("org.postgresql.Driver");
-    dataSource.setUrl("jdbc:postgresql://192.168.15.45:5432/SambaWeb");
-    dataSource.setUsername("postgres");
-    dataSource.setPassword("postgres");
+    dataSource.setUrl(driver + "://" + ip + ":" + port + "/" + database);
+    dataSource.setUsername(username);
+    dataSource.setPassword(password);
     return dataSource;
+  }
+
+  public void setDriver(String driver) {
+    this.driver = driver;
+  }
+
+  public void setIp(String ip) {
+    this.ip = ip;
+  }
+
+  public void setPort(String port) {
+    this.port = port;
+  }
+
+  public void setDatabase(String database) {
+    this.database = database;
+  }
+
+  public void setUsername(String username) {
+    this.username = username;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
+
+  public String getDriver() {
+    return driver;
+  }
+
+  public String getIp() {
+    return ip;
+  }
+
+  public String getPort() {
+    return port;
+  }
+
+  public String getDatabase() {
+    return database;
+  }
+
+  public String getUsername() {
+    return username;
+  }
+
+  public String getPassword() {
+    return password;
   }
 }
---
+++ SambaWebAPI/src/test/java/com/br/SambaWebAPI/testIntegration/logs/LogIntegrationTest.java
@@ -0,0 +1,55 @@
+package com.br.SambaWebAPI.testIntegration.logs;
+
+import com.br.SambaWebAPI.SambaWebApiApplication;
+import com.br.SambaWebAPI.config.DataSourceConfig;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.sql.*;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = SambaWebApiApplication.class)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class LogIntegrationTest {
+
+    @Autowired
+    private DataSourceConfig dataSourceConfig;
+
+
+    @Test
+    public void insertLogData() throws SQLException {
+        try (Connection connection = dataSourceConfig.dataSource().getConnection()) {
+
+            String query = "INSERT INTO logs (log_description) VALUES ('Teste 1')";
+
+            try (PreparedStatement statement = connection.prepareStatement(query, Statement.RETURN_GENERATED_KEYS)) {
+                statement.execute();
+
+                try (ResultSet generatedKeys = statement.getGeneratedKeys()) {
+                    if (generatedKeys.next()) {
+                        int id = generatedKeys.getInt(1);
+                        query = "SELECT log_description FROM logs WHERE id = ?";
+
+                        try (PreparedStatement selectStatement = connection.prepareStatement(query)) {
+                            selectStatement.setInt(1, id);
+
+                            try (ResultSet resultSet = selectStatement.executeQuery()) {
+                                while (resultSet.next()) {
+                                    String logDescription = resultSet.getString("log_description");
+
+                                    assertEquals("Teste 1", logDescription);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
